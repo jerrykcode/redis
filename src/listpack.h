249ab74292f8f81d18847c34a080aef103f30554
Mingyi Kang <jerrykang026@gmail.com>
2025-05-18 16:05:28 +0800

    Implement lpDeleteRangeWithEntryPtr (Copy from #13132)

--- src/listpack.h
+++ src/listpack.h
@@ -49,6 +49,7 @@ unsigned char *lpReplace(unsigned char *lp, unsigned char **p, unsigned char *s,
 unsigned char *lpReplaceInteger(unsigned char *lp, unsigned char **p, long long lval);
 unsigned char *lpDelete(unsigned char *lp, unsigned char *p, unsigned char **newp);
 unsigned char *lpDeleteRangeWithEntry(unsigned char *lp, unsigned char **p, unsigned long num);
+unsigned char *lpDeleteRangeWithEntryPtr(unsigned char *lp, unsigned char **first, unsigned char *tail, unsigned long num);
 unsigned char *lpDeleteRange(unsigned char *lp, long index, unsigned long num);
 unsigned char *lpBatchAppend(unsigned char *lp, listpackEntry *entries, unsigned long len);
 unsigned char *lpBatchInsert(unsigned char *lp, unsigned char *p, int where,
